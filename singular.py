f109f7b50700d7da05ed03dce3ec459f4e4d1375
Nathan Harris, Jr. <nathanharris29@gmail.com>
2024-08-22 08:07:07 -0500

    Add files via upload

---
+++ singular.py
@@ -0,0 +1,19 @@
+from mininet.topo import Topo
+
+class SingularTopo(Topo):
+
+    def __init__(self, n=100):
+        # Initialize topology
+        Topo.__init__(self)
+        h = []
+        s = ['s1']
+        self.addSwitch('s1')
+
+        for i in range(1, n+1):
+            str_h = 'h' + str(i)
+            h.append(self.addHost(str_h))
+
+            self.addLink(h[-1], s[-1])
+
+
+topos = {'singular': (lambda: SingularTopo())}
